df366ec88e3746d3d788bf4070d769e7365f1887
trandinhthanh <trandinhthanh04091997it@gmail.com>
2022-04-15 16:49:52 +0700

    finish code

--- assets/main.js
+++ assets/main.js
@@ -5,37 +5,49 @@ const closeModal = document.querySelector('.modal-close')
 const modalContainer = document.querySelector('.js-modal-container')
 
 //OpenModal
-function showFormTicket(){  
+function showFormTicket() {
     modal.classList.add('open-modal')
 }
+
 //CloseModal
-function hideForm(){
+function hideForm() {
     modal.classList.remove('open-modal')
 }
 
-for(const buyBtn of buyBtns){
-    buyBtn.addEventListener('click',showFormTicket)
+for (const buyBtn of buyBtns) {
+    buyBtn.addEventListener('click', showFormTicket)
 }
-closeModal.addEventListener('click',hideForm )
+closeModal.addEventListener('click', hideForm)
 
-modal.addEventListener('click',hideForm)
-modalContainer.addEventListener('click',function(event){
+modal.addEventListener('click', hideForm)
+modalContainer.addEventListener('click', function (event) {
     event.stopPropagation()
 })
 
-
 //Close|Open Menu
-
 const hiddenMenu = document.getElementById('hidden-menu')
 const elementNav = document.getElementById('nav')
 
-hiddenMenu.addEventListener('click',function() {
-   if (elementNav.classList.contains('open-hidden-menu') == false) {
+hiddenMenu.addEventListener('click', function () {
+    if (elementNav.classList.contains('open-hidden-menu') == false) {
         elementNav.classList.add('open-hidden-menu')
-   } else {
+    } else {
         elementNav.classList.remove('open-hidden-menu')
-   }
+    }
 })
 
+//auto close when click menuItems
+const elementItemsMenu = document.querySelectorAll('#nav li a')
+for (let index = 0; index < elementItemsMenu.length; index++) {
+    elementItemsMenu[index].addEventListener('click', function (event) {
+        //dùng nextElementSibling để lấy ra node <ul> sau đó kiểm tra
+        isSubnav = elementItemsMenu[index].nextElementSibling && elementItemsMenu[index].nextElementSibling.classList.contains('subnav')
+        if (isSubnav) {
+            event.preventDefault();
+        } else {
+            elementNav.classList.remove('open-hidden-menu')
+        }
+    })
+}
 
 
